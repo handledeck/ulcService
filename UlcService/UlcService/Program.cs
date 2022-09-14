feadd5a21bd46b083fee15c2316040680ebbf3c1
Дронов Олег Александрович <O.Dronov@vitebsk.energo.net>
2022-09-14 20:16:45 +0300

    test

--- UlcService/UlcService/Program.cs
+++ UlcService/UlcService/Program.cs
@@ -13,12 +13,18 @@ namespace UlcService
   {
     static void Main(string[] args)
     {
-      StreamWriter s = new StreamWriter(@"d:\123.ini",false);
+      ReadIni();
+      //.WriteData(s,)
+    }
+
+
+    static void WriteIni() {
+      StreamWriter s = new StreamWriter(@"d:\123.ini", false);
       //IniParser.Model.SectionData data = new IniParser.Model.SectionData("one");
       IniParser.Model.SectionDataCollection sectionDatas = new IniParser.Model.SectionDataCollection();
       IniParser.Model.SectionData db = new IniParser.Model.SectionData("DB");
       db.Comments.Add("test to connection");
-      db.Keys.AddKey("ip","127.0.0.1");
+      db.Keys.AddKey("ip", "127.0.0.1");
       db.Keys.AddKey("port", "5432");
       //db.Keys.AddKey("user", "");
       IniParser.Model.IniData iniData = new IniParser.Model.IniData();
@@ -27,8 +33,17 @@ namespace UlcService
       fileIniDataParser.WriteData(s, iniData);
       s.Flush();
       s.Close();
-      //.WriteData(s,)
     }
+
+    static void ReadIni() {
+      StreamReader s = new StreamReader(@"d:\123.ini", false);
+      IniParser.Parser.IniDataParser p= new IniDataParser();
+      IniParser.FileIniDataParser fileIniDataParser = new IniParser.FileIniDataParser();
+      var iData=fileIniDataParser.ReadData(s);
+      var ip=iData["DB"].GetKeyData("ip").Value;
+
+    }
+
     static void CreateDb()
     {
       string connection = string.Format("Host={0};Port={1};Username={2};Password={3};Database=''",
